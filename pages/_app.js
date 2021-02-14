57e1451ccd244d853aedc23ade34c1478fc24abd
sigrundis <sigrundish@gmail.com>
2021-02-14 18:39:21 +0100

    add language parameter to html for screen readers

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
 import React, { useEffect } from 'react';
 import '../styles/globals.scss';
 import loadFont from '../lib/loadFont';
+import { isBrowser } from '../lib/helpers';
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
     loadFont();
+
+    if (isBrowser) {
+      // For screen readers, to increase accessability.
+      document.documentElement.lang = 'en';
+    }
   }, []);
 
   return <Component {...pageProps} />;
